d9e44937a63355adf7c4e798dbbc40e14b910229
Eric Talevich <eric.talevich@gmail.com>
2012-11-01 14:35:37 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"Fammer: Tools for protein superfamily sequence profile alignment and curation."
+
+from os.path import dirname, join
+
+DIR = (dirname(__file__) or '.')
+
+setup_args = dict(
+    name='fammer',
+    version='0.1.0',
+    description=__doc__,
+    author='Eric Talevich',
+    author_email='eric.talevich@gmail.com',
+    url='http://github.com/etal/fammer',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    packages=['fammer'],
+    scripts=[join(DIR, 'fammer.py'), join(DIR, 'tmalign.py')],
+)
+
+try:
+    from setuptools import setup
+    setup_args.update(
+        install_requires=[
+            'biopython >= 1.58',
+            'networkx >= 1.0',
+            'biocma >= 0.1.0',
+            'biofrills >= 0.1.0',
+        ])
+except:
+    from distutils.core import setup
+
+setup(**setup_args)
+
